7da3e47a64b6c94c28f63cbb2f53738aece199dd
reflake <reflakeykt@gmail.com>
2021-03-03 20:57:54 +0900

    laser sight aim of enemy ball

--- Assets/Scripts/EnemyBall.cs
+++ Assets/Scripts/EnemyBall.cs
@@ -1,25 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBall : MonoBehaviour
 {
+    public LineRenderer Sight;
+    public float AimRange = 3.5f;
+    
+    private Ball Target;
     private float coolDown = 5f;
-    public Ball player;
-
-    void Update()
-    {
-        Vector3 targetDirection = player.transform.position - transform.position;
-
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, 1f * Time.deltaTime, 0.5f);
-
-        transform.rotation = Quaternion.LookRotation(newDirection);
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Ball")
+        if (collision.gameObject.tag == "Player")
         {
             Destroy(this.gameObject);
         }
@@ -42,13 +33,21 @@ public class EnemyBall : MonoBehaviour
         if (Target)
         {
             // Show sight
+            var targetOffset = Target.transform.position - transform.position;
+            
             Sight.positionCount = 2;
-            Sight.SetPositions(new[] {transform.position, Target.position});
+            Sight.SetPositions(new[] {Vector3.zero, Vector3.forward * targetOffset.magnitude / transform.localScale.x});
 
             if ( ! IsInSight(Target) )
             {
                 Target = null;
             }
+            
+            // Rotate towards player
+            Vector3 targetDirection = targetOffset;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, 1f * Time.deltaTime, 0.5f);
+
+            transform.rotation = Quaternion.LookRotation(newDirection);
         }
         else
         {
@@ -61,14 +60,14 @@ public class EnemyBall : MonoBehaviour
     {
         foreach (var ball in GameObject.FindGameObjectsWithTag("Player"))
         {
-            if (IsInSight(ball.transform))
+            if (IsInSight(ball.GetComponent<Ball>()))
             {
-                Target = ball.transform;
+                Target = ball.GetComponent<Ball>();
             }
         }
     }
 
-    private bool IsInSight(Transform ball)
+    private bool IsInSight(Ball ball)
     {
         Vector3 ballOffset = ball.transform.position - transform.position;
 
