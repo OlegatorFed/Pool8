1444b156ef47316cb315ab544dee8e0f3d12c4d0
reflake <reflakeykt@gmail.com>
2021-03-14 19:17:35 +0900

    ✨ NEW! 45 degree camera

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -20,7 +20,8 @@ public class CamControl : MonoBehaviour
         float RightButton = Input.GetKey("d") || Input.mousePosition.x >= Screen.width - PanBorder ? 1 : 0;
         float LeftButton = Input.GetKey("a") || Input.mousePosition.x <= PanBorder ? -1 : 0;
 
-        pos += new Vector3(RightButton + LeftButton, 0, UpButton + DownButton) * PanSpeed * Time.deltaTime;
+        pos += transform.right * (RightButton + LeftButton) * PanSpeed * Time.deltaTime;
+        pos += transform.forward * (UpButton + DownButton) * PanSpeed * Time.deltaTime;
 
         pos.x = Mathf.Clamp(pos.x, -PanLimit.x, PanLimit.x);
         pos.z = Mathf.Clamp(pos.z, -PanLimit.y, PanLimit.y);
