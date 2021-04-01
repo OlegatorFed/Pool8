9518110df3e0de23fb7d193f11f0950edb04563d
reflake <reflakeykt@gmail.com>
2021-04-01 13:47:21 +0900

    ✨ NEW! dynamic camera that follows player

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -1,31 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CamControl : MonoBehaviour
 {
+    public float VelocityMulitplier = 3f;
     public float PanSpeed = 20f;
-    public float PanBorder = 15f;
-    public Vector2 PanLimit;
+    public float FovSmooth = 2f;
+    public float MaxFov = 64;
+    public float MinFov = 30;
+    public float MaximalVelocity = 40f;
+    public float MinimalVelocity = 12f;
 
+    private Camera _camera = null;
+    private Rigidbody _target = null;
+    private Vector3 _cameraVelocity = Vector3.zero;
+    private float _fovVelocity = 0f;
+    
+    public void Awake()
+    {
+        _camera = Camera.main;
+
+        _target = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+
+        transform.position = _target.position;
+    }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        Vector3 pos = transform.position;
+        Vector3 desiredPosition = _target.position + _target.velocity * VelocityMulitplier;
 
-        //too bad...
-        float UpButton = Input.GetKey("w") || Input.mousePosition.y >= Screen.height - PanBorder ? 1 : 0;
-        float DownButton = Input.GetKey("s") || Input.mousePosition.y <= PanBorder ? -1 : 0;
-        float RightButton = Input.GetKey("d") || Input.mousePosition.x >= Screen.width - PanBorder ? 1 : 0;
-        float LeftButton = Input.GetKey("a") || Input.mousePosition.x <= PanBorder ? -1 : 0;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref _cameraVelocity, PanSpeed);
 
-        pos += transform.right * (RightButton + LeftButton) * PanSpeed * Time.deltaTime;
-        pos += transform.forward * (UpButton + DownButton) * PanSpeed * Time.deltaTime;
+        float velocityNormalized = (_target.velocity.magnitude - MinimalVelocity) / (MaximalVelocity - MinimalVelocity);
+        float desiredFov = Mathf.Lerp(MinFov, MaxFov, velocityNormalized);
 
-        pos.x = Mathf.Clamp(pos.x, -PanLimit.x, PanLimit.x);
-        pos.z = Mathf.Clamp(pos.z, -PanLimit.y, PanLimit.y);
-
-        transform.position = pos;
+        _camera.fieldOfView = Mathf.SmoothDamp(_camera.fieldOfView, desiredFov, ref _fovVelocity, FovSmooth);
     }
 }
