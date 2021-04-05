8f16c002eee1120171e3dc80266f650a89308e92
reflake <reflakeykt@gmail.com>
2021-04-05 16:11:37 +0900

    🔨 FIX. bump jump bug occure less likely

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,16 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     public new Rigidbody rigidbody;
     public bool IsStill => rigidbody.velocity.magnitude <= 0.09f;
 
+    public float SphereRadius = 0.2f;
+    public float SphereFeet = 0.3f;
+    public float FeetThreshold = 0.03f;
+
+    private Plane feetPlane;
+    
     // Update is called once per frame
     void Update()
     {
         if (this.transform.position.y < 0 - this.transform.GetComponent<Collider>().bounds.size.y && this.tag == "Player")
         {
             Gameplay.instance.PlayerGetsKilled(this.gameObject);
+        }
+        
+        RaycastHit hitInfo;
+        Ray ray = new Ray();
+        ray.origin = transform.position;
+        ray.direction = Vector3.down;
+
+        if (Physics.Raycast(ray, out hitInfo, SphereFeet + FeetThreshold))
+        {
+            var newVelocity = rigidbody.velocity;
+
+            newVelocity.y = 0;
+            
+            transform.position = hitInfo.point + Vector3.up * SphereFeet;
+            rigidbody.velocity = newVelocity;
         }
     }
 
@@ -22,5 +44,9 @@ public class Ball : MonoBehaviour
     public void Revive()
     {
         gameObject.SetActive(true);
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
     }
 }
